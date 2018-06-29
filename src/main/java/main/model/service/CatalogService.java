3bdfb33c6014683ad01327086e57e2e339cb3130
maestro <maestrokd.reg@gmail.com>
2018-06-30 00:54:44 +0300

    refactor: Refactor backend #CCX-1

--- src/main/java/main/model/service/CatalogService.java
+++ src/main/java/main/model/service/CatalogService.java
@@ -1,17 +1,18 @@
 package main.model.service;
 
 import main.model.entity.Catalog;
-import main.model.entity.CatalogPageResource;
+import main.model.entity.Cd;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.List;
 
 public interface CatalogService {
+    List<Cd> getAllCds() throws IOException;
     Catalog getCatalog() throws IOException;
-    String getCatalogAsXmlString() throws IOException;
-    Resource getCatalogAsResource() throws MalformedURLException;
+    String getCatalogAsString() throws IOException;
+    Resource getFileOnServerAsResource() throws MalformedURLException;
     String getCatalogAsPageResourceString(int page, int size) throws IOException;
     void addUploadedMultipartFileToServerFile(MultipartFile multipartFile) throws IOException;
 }
