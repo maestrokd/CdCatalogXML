f4cd0bf8798e1212648dff3bf249dc93e0335a3e
maestro <maestrokd.reg@gmail.com>
2018-06-30 11:03:49 +0300

    refactor: Refactor backend

--- src/main/java/main/model/entity/Catalog.java
+++ src/main/java/main/model/entity/Catalog.java
@@ -3,8 +3,10 @@ package main.model.entity;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Data
 @JacksonXmlRootElement(localName = "CATALOG")
--- src/main/java/main/model/service/CatalogService.java
+++ src/main/java/main/model/service/CatalogService.java
@@ -4,15 +4,21 @@ import main.model.entity.Catalog;
 import main.model.entity.Cd;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.List;
 
 public interface CatalogService {
     List<Cd> getAllCds() throws IOException;
+
     Catalog getCatalog() throws IOException;
+
     String getCatalogAsString() throws IOException;
+
     Resource getFileOnServerAsResource() throws MalformedURLException;
+
     String getCatalogAsPageResourceString(int page, int size) throws IOException;
+
     void addUploadedMultipartFileToServerFile(MultipartFile multipartFile) throws IOException;
 }
